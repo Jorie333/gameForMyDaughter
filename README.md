6c4bda1216e087bda862638ee61e9b430ac6c828
Jorie333 <68300820+Jorie333@users.noreply.github.com>
2020-07-29 07:45:51 -0400

    I'm learning Python and attempting to use my new skills to create a simple game for my daughter that would allow her to choose a name for our computer.  I am struggling with conditional variables and would love guidance!
    This short game is intended to engage the player in dialogue and invite them to choose a name for her/his computer

---
+++ README.md
@@ -0,0 +1,2 @@
+# gameForMyDaughter
+ I'm learning Python and attempting to use my new skills to create a simple game for my daughter.
---
+++ iLoveEila.py
@@ -0,0 +1,12 @@
+print ("Hello! I'm your computer")
+name = input ("What's your name?")
+print (name,"is a pretty name. I wish I had a name. Would you like to give me one?")
+answer = input ("yes or no?")
+if answer="yes" #this is where my code blows up
+    print ("Great! Let's get started!")
+elif answer = "Yes"
+    print ("Great! Let's get started!")
+elif answer = "YES"
+    print ("Great! Let's get started!")
+else
+    print ("Oh. That's too bad. I guess I'll go now. Have a good day!")
